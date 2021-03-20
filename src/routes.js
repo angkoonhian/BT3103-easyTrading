a173a496a712e316f1657190421221b9c14c5a42
desmondaw <gimper.ds@gmail.com>
2021-03-20 20:27:29 +0800

    displayed  sales listings in card form

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@
 import NewListing from './components/NewListing.vue'
 import Login from './components/Login.vue'
 import SignUp from './components/SignUp.vue'
+import Sales from './components/Sales.vue'
 
 export default [
   { path: '/', component: NewListing },
   { path: '/login', component: Login },
   { path: '/newListing', name:'newListing',component: NewListing},
-  { path: '/Signup', name:'signUp',component: SignUp}
+  { path: '/Signup', name:'signUp',component: SignUp}, 
+  { path: '/sales', name:'sales',component: Sales}, 
 ]
