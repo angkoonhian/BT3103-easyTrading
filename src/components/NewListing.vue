8c287b37fdecb731598d6ba40587a7dda824f74a
desmondaw <gimper.ds@gmail.com>
2021-03-24 22:55:21 +0800

    added ability to upload images in newlisting form

--- src/components/NewListing.vue
+++ src/components/NewListing.vue
@@ -19,24 +19,36 @@
         <h2><i>4. Describe your listing</i></h2><br>
             <textarea type='text' maxlength="50" rows="8" cols = '70' v-model='desc'></textarea>
         <h2><i>5. Location where you want to deal</i></h2><br>
-        <input type='text' id='listingloc' maxlength="50" size="70" placeholder="no more than 50 chars" v-model="loc">
+            <input type='text' id='listingloc' maxlength="50" size="70" placeholder="no more than 50 chars" v-model="loc">
+        <h2><i>6. Upload your pictures:</i></h2><br>          
+            <div>
+                <div>
+                <button @click="click1">choose a photo</button>
+                <input type="file" ref="input1"
+                    style="display: none"
+                    @change="previewImage" accept="image/*" >                
+                </div>
+                <div v-if="imageData!=null">                     
+                    <img class="preview" height="268" width="356" :src="img1"><br>
+                </div> 
+            </div>     
     </div>
     <div id = "additionalOptions" v-show="selectedType==='sale'">
-        <h2><i>6. Name your price and/or trades</i></h2><br>
+        <h2><i>7. Name your price and/or trades</i></h2><br>
         $ <input type="number" step=0.01 v-model = "price"><br>
         -or-<br>
         <input type='text' maxlength="50" size="70" v-model = "alt_trade">
-        <h2><i>7. Name your transaction/delivery preferences</i></h2><br>
+        <h2><i>8. Name your transaction/delivery preferences</i></h2><br>
         <input type='text' placeholder="e.g. pick up at mrt, cash only, etc" size="70" v-model="trans_method"><br>
         <button v-on:click="submitListing('sale')">Submit</button>
     </div>
     <div id = "additionalOptions" v-show="selectedType==='rent'">
-        <h2><i>6. Name your price</i></h2><br>
+        <h2><i>7. Name your price</i></h2><br>
         $ <input type="number" step=0.01 v-model="price"> per 
         <select v-model="interval" required> 
             <option v-for="x in rent_intervals" :key="x.id" v-bind:value="x" selected = 'selected'>{{x}}</option>
         </select>
-        <h2><i>7. Name your rules</i></h2><br>
+        <h2><i>8. Name your rules</i></h2><br>
         <textarea type='text' maxlength="50" rows="8" cols = '70' v-model="tnc"></textarea><br>
         <button v-on:click="submitListing('rent')">Submit</button>
     </div>
@@ -45,6 +57,7 @@
 
 <script>
 import Header from '../components/Header'
+import firebase from 'firebase'
 
 export default {
     components:{
@@ -67,7 +80,9 @@ export default {
             rent_intervals: ['hour', 'day', 'week', 'month', 'year'], 
             interval: '', 
             tnc: '', 
-            
+            img1: '',
+            imageData: null, 
+            imgurls: []
         }
     }, 
     methods: {
@@ -90,7 +105,47 @@ export default {
                 this.listing['rent'] = others2; 
             }
             console.log(this.listing)
-        }
+        }, 
+        // create () {
+        //     const post = {
+        //         photo: this.img1,
+        //         caption: this.caption        
+        //     }
+        //     firebase.database().ref('PhotoGallery').push(post)
+        //     .then((response) => {
+        //         console.log(response)
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
+        // },
+        click1() {
+            this.$refs.input1.click()   
+        },
+        previewImage(event) {
+            this.uploadValue=0;
+            this.img1=null;
+            this.imageData = event.target.files[0];
+            this.onUpload()
+        },
+        onUpload(){
+            this.img1=null;
+            const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+            storageRef.on(`state_changed`,
+            snapshot=>{
+                this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            }, 
+            error=>{console.log(error.message)},
+            ()=>{
+                this.uploadValue=100;
+                storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+                this.img1 =url;
+                this.imgurls.push(this.img1); 
+                console.log(this.imgurls); 
+                });
+            }      
+            );
+        },
     }
 }
 </script>
