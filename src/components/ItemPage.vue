fadebdba735cc426509e593718a6f9951fcc905e
e0407464 <79753330+e0407464@users.noreply.github.com>
2021-04-05 17:46:42 +0800

    updated item page
    previous one has text i testing with

--- src/components/ItemPage.vue
+++ src/components/ItemPage.vue
@@ -2,7 +2,6 @@
   <div>
     <br />
     <br />
-    <p>{{listing}}</p>
     <h1 v-for="(x,i) in itemInfo" v-bind:key="i">
         {{x[1].Title}} for {{x[1].Type}}
     </h1>
@@ -11,11 +10,6 @@
         <v-col v-for="(x,i) in itemInfo" v-bind:key="i" md="12" >
             <v-card color ='grey lighten-4'>
                 <v-card-text>
-                    <div>
-                        Word of the Day
-                        {{x[1].Type}}
-                        
-                    </div>
                     <v-row no-gutters>
                         <v-col md="6">
                             <v-card color ='grey lighten-4'>
